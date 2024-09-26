4aa5a75be01793fdb49001a5700165fbbf68824f
rianaditro <rianaditro@gmail.com>
2024-09-26 22:12:18 +0700

    add output checker

--- file_checker.py
+++ file_checker.py
@@ -1,8 +1,34 @@
 import pandas as pd
+import numpy as np
 
 
-df = pd.read_excel("dental.xlsx")
+df = pd.read_excel("clean_data.xlsx", index_col=0)
+print(len(df))
 
-print(df["valid"].value_counts())
+# remove duplicate
+df = df.drop_duplicates(subset=["nama", "no. telp"], keep="first")
+# remove empty
+df = df.dropna(how='all').replace('', np.nan).dropna(how='all')
 
-print(df.isnull().sum())
+# reformat no. telp
+to_remove = {
+    "-": "",
+    "\(": "",
+    "\)": "",
+    " ": ""
+}
+
+df["no. telp"] = df["no. telp"].replace(to_remove, regex=True)
+
+# remove valid column
+df = df.drop(columns=["valid"])
+
+print(len(df))
+
+# save to excel
+df = df.reset_index(drop=True)
+df.index = df.index + 1
+
+df.to_excel("dental3.xlsx", index_label="index")
+
+print(df.info())
